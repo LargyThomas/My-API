a97aa2a7647ff9c16e42236851ba74dd54a54440
ShinoVesper <largythomas@gmail.com>
2026-09-14 09:36:25 +0200

    feat(app): initialize express app and mount auth endpoints

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 
+// Encode the request body as JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/health', (req, res) => {
     res.status(200).send('ok\n');
 });
 
 app.use('/animals', require('./features/animals/animals.routes'));
-// app.use('/auth', require('./features/auth/auth.routes'));
+app.use('/auth', require('./features/auth/auth.routes'));
 
 module.exports = app;
